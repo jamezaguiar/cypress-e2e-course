2b05b0609ed8ec686b6b8f3f20f651c1d5e3acc0
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-04-10 22:04:29 -0300

    selecting by text

--- 01-cypress-basics/cypress/e2e/basics.cy.js
+++ 01-cypress-basics/cypress/e2e/basics.cy.js
@@ -3,5 +3,11 @@
 describe('tasks page', () => {
   it('should render the main image', () => {
     cy.visit('http://localhost:5173/');
+    cy.get('.main-header img');
+  });
+
+  it('should display the page title', () => {
+    cy.visit('http://localhost:5173/');
+    cy.get('.main-header h1').contains(/my cypress course tasks/i);
   });
 });
